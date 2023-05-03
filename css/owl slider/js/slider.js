f95ae70c8b072797d70637031c456457e534b661
trushiltejani27 <trushiltejani27@gmail.com>
2023-05-03 09:34:30 +0530

    first commit

---
+++ css/owl slider/js/slider.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 2,
+        margin:20,
+        loop:true,
+        center:true,
+        // mouseDrag:false,
+        nav:true,
+        // autoplay:true,
+        // autoplayTimeout:2000,
+        // autoplayHoverPause:true,
+        // animateIn:'animate__backInDown',
+        // animateOut:'animate__backOutDown',
+        // navText: ['<i class="fa fa-arrow-left" aria-hidden="true"></i>','<i class="fa fa-arrow-right" aria-hidden="true"></i>']
+    });
+});
---
+++ css/owl slider/js/slider3.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        margin:20,
+        loop:true,
+        center:true,
+        // mouseDrag:false,
+        nav:true,
+        // autoplay:true,
+        // autoplayTimeout:2000,
+        // autoplayHoverPause:true,
+        // stagePadding:50,
+        // rewind:true,
+        // animateIn:'animate__backInDown',
+        // animateOut:'animate__backOutDown',
+    });
+});
